272dfc0c319d7424d09163d6f8cab89770c8d52d
Julianka-GJ <julia.garmarchuk@gmail.com>
2022-02-19 20:34:26 +0200

    finally got it!

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -2,6 +2,7 @@
 
 var n = 10;
 var m = 10;
+Sumcol = [];
 var A = new Array(n);
 
 for (var row = 0; row < A.length; row++) {
@@ -29,16 +30,23 @@ for (var row = 0; row < A.length; row++) {
 }*/
 
 
-for (var _row = 0; _row < A.length; _row++) {
+for (var _col = 0; _col < A.length; _col++) {
   Sum = 0;
 
-  for (var _col = 0; _col < A[_row].length; _col++) {
-    Sum += A[_col][_row]; //Сумма колонок 
+  for (var _row = 0; _row < A[_col].length; _row++) {
+    Sum += A[_row][_col]; //Сумма колонок 
   }
 
-  if (Sum < 0) {
-    for (var k = A.length - 1; k >= 0; k--) {
-      A[k].splice(_row, 1);
+  Sumcol.push(Sum);
+  console.log(Sumcol);
+}
+
+for (var k = A.length - 1; k >= 0; k--) {
+  for (var _row2 = 0; _row2 < A.length; _row2++) {
+    if (Sumcol[k] < 0) {
+      console.log(Sumcol[k]);
+
+      A[_row2].splice(k, 1);
     }
   }
 }
@@ -46,9 +54,9 @@ for (var _row = 0; _row < A.length; _row++) {
 console.log(A);
 document.write('<br>');
 
-for (var _row2 = 0; _row2 < A.length; _row2++) {
-  for (var _col2 = 0; _col2 < A[_row2].length; _col2++) {
-    document.write(A[_row2][_col2] + ' ');
+for (var _row3 = 0; _row3 < A.length; _row3++) {
+  for (var _col2 = 0; _col2 < A[_row3].length; _col2++) {
+    document.write(A[_row3][_col2] + ' ');
   }
 
   document.write('<br>');
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const n = 10;
 const m = 10;
+Sumcol = [];
 
 let A = new Array(n);
 
@@ -27,32 +28,37 @@ for (let row=0; row<A.length; row++) {
     }
 }*/
 
-
-for (let row=0; row<A.length; row++){
+for (let col=0; col<A.length; col++){
     Sum = 0;      
-    for (let col=0; col<A[row].length; col++) {
-        Sum += A[col][row]; //Сумма колонок 
+    for (let row=0; row<A[col].length; row++) {
+        Sum += A[row][col]; //Сумма колонок 
     }
 
-    if (Sum < 0 ) {
-        for (let k=A.length-1; k>=0; k--) {  
-                A[k].splice(row, 1); 
+    Sumcol.push(Sum);
+    console.log(Sumcol);
+}
+
+for (let k=A.length-1; k>=0; k--) {
+    for (let row=0; row<A.length; row++) {  
+        if (Sumcol[k] < 0 ) {
+            console.log(Sumcol[k]);
+            A[row].splice(k, 1); 
         }
     }
 }
 
 
 console.log(A); 
-
 document.write('<br>');
 
 
 for (let row=0; row<A.length; row++){
     for (let col=0; col<A[row].length; col++) {
         document.write(A[row][col] + ' ');
-}
+    }
         document.write('<br>');
 }
+
 
 
 
